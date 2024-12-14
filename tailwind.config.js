3dd1afa560f2ec2060269192014975ca2fde6a89
Omesh Sana <omesh.reddy18@gmail.com>
2024-12-13 22:25:44 -0600

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        'sunflower-gold': '#FFD700',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
